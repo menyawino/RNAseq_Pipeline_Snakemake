d7496b896c05dde2e48c1c8668848423c92e2666
menyawino <omarahmedd@aucegypt.edu>
2024-10-13 11:53:40 +0300

    started updating ballgown.r script

--- workflow/scripts/ballgown.r
+++ workflow/scripts/ballgown.r
@@ -15,14 +15,10 @@ bg <- ballgown(dataDir=args[1], samplePattern=args[2], meas='all')
 bg_filt <- subset(bg, "rowVars(texpr(bg)) > 1", genomesubset=TRUE)
 
 # Run the differential expression analysis
-
 bg_filt <- stattest(bg_filt, feature='gene', covariate=args[3])
 
 # Write the results to a file
-
 write.table(bg_filt, file=args[4], sep="\t", quote=FALSE, row.names=FALSE)
 
 # Plot the results
-
 plotData <- merge(as.data.frame(transcripts(bg_filt)), as.data.frame(genes(bg_filt)), by="gene_id")
-
